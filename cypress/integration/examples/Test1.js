c7e9b14aa5b24032a9f23934cb6e2cf17fb2460c
sanyi8 <51482271+sanyi8@users.noreply.github.com>
2023-03-07 14:22:57 +0000

    adding test tasks

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,16 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', () => {
   it('My first test case', () => {
     
     cy.visit("https://www.amazon.co.uk/");
     //fixture
+    //Accept Button
+    cy.get("#a-autoid-0").click()
+    //Search for rc car
+    cy.get('#twotabsearchtextbox').type("rc car")
+    cy.get('#nav-search-submit-button').click()
+    //for item is on the first promo products
+    cy.get('._bGlmZ_item_awNhH').should('have.length',3)
   })
 })
