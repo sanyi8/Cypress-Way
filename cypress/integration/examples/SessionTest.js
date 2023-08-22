2fb1a9bb3c122d09b4de84de379d7bf1084c08b5
Sandor Kardos <sanyitelo@gmail.com>
2023-08-22 14:52:28 +0100

    selecting from dropdown

--- cypress/integration/examples/SessionTest.js
+++ cypress/integration/examples/SessionTest.js
@@ -1,7 +1,7 @@
 // [routerlink*='cart']
 describe('JWT Session', () => {
     it('is logged in through local storage', () => {
-        cy.LoginAPI().then(function()
+        cy.LoginAPI().then(function()   // for the login token
         {
            cy.visit("https://rahulshettyacademy.com/client",
            {
@@ -10,6 +10,31 @@ describe('JWT Session', () => {
                     window.localStorage.setItem('token',Cypress.env('token'))
                 }
            })
+           
         })
+        cy.get(".card-body button:last-of-type").eq(1).click() //1 select second "add to cart" button
+        cy.get("[routerlink*='cart']").click();
+        cy.contains("Checkout").click()     //chose an UI element$$$ by name "Checkout"
+        cy.get("[placeholder='Select Country']").type("ind");
+
+        // create a loop for iterate thro dropdown menu and pick india
+        cy.get(".ta-results button").each(($el,index,$list)=>
+        {
+            if($el.text === "India")
+            {
+                // when I find "India I wrap then click"
+                cy.wrap($el).click()
+
+            }
+        })
+        
+        // wait only here as Rahul server slow DO NOT USE IN REAL
+        // chose India button which is the 3rd it should be by above I loop
+        cy.wait(800);
+        cy.get('.ta-results > :nth-child(3)').click()
+        cy.get(".action__submit").click();
+        
+
+
     })
 })
